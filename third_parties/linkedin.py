6706a8dd3cbc8ce05c60fa0023a45ca24ddb1316
monisharajan <monisha.rajan@thoughtworks.com>
2023-07-09 00:14:19 +0530

    Adding llm code for fetching twitter details

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -1,7 +1,5 @@
 import os
 import requests
-
-
 def scrape_linkedin_profile(linkedin_profile_url: str):
     """scrape information from LinkedIn profiles,
     Manually scrape the information from the LinkedIn profile"""
